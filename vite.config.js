fc000030f0453a707206fbc14887998369112f86
SolutionsCProjetos <SolutionsCProjetos@hotmail.com>
2025-08-27 20:06:41 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/irl/",
+  // base: "/irl/",
   plugins: [react()],
   optimizeDeps: {
     include: ['pdfmake/build/pdfmake', 'pdfmake/build/vfs_fonts']
