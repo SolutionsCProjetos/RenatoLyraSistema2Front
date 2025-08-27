b06fa495a354864b6529fabd7b2c10272a314c54
SolutionsCProjetos <SolutionsCProjetos@hotmail.com>
2025-08-27 19:29:08 -0300

    Add files via upload

---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,19 @@
+function Erro() {
+    return (
+        <div className="flex ">
+            <div className="flex flex-col flex-grow">
+                <div className="">
+                    <div className="flex justify-center items-center w-full h-full">
+                        <div className="text-center">
+                            <h2>
+                                Ops... Essa pagina não existe!
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Erro;
